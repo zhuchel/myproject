6b9fbc2b182aa2f92e4a446578f1e3c1036c70a8
zhuchel <zhuchel@hotmail.com>
2016-02-09 21:54:01 +0100

    add

--- server.R
+++
@@ -1,7 +0,0 @@
-diabetesRisk <- function(glucose) glucose / 200
-shinyServer(
-        function(input, output) {
-                output$inputValue <- renderPrint({input$range})
-                output$prediction <- renderPrint({diabetesRisk(input$glucose)})
-        }
-)
