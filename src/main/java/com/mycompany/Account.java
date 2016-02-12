c7a77d655c31c935bbb89dac2cf8df0583537bee
zhuchel <zhuchel@hotmail.com>
2016-02-12 19:12:02 +0100

    add dummy

---
+++ src/main/java/com/mycompany/Account.java
@@ -0,0 +1,27 @@
+package com.mycompany;
+
+
+public class Account {
+
+  private String name;
+
+  private float amount;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public float getAmount() {
+    return amount;
+  }
+
+  public void setAmount(float amount) {
+    this.amount = amount;
+  }
+
+
+}
